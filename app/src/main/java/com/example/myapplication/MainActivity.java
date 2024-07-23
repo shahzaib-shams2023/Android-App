280e00823285319d336bd98d3ce73b5fbb2db02a
shahzaib-shams <shahzaib@aptechgdn.net>
2024-07-24 03:15:33 +0500

    files

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private List<Product> mProductList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Obtain a reference to the product catalog
+        mProductList = ShoppingCartHelper.getCatalog(getResources());
+
+        // Create the list
+        ListView listViewCatalog = (ListView) findViewById(R.id.ListViewCatalog);
+        listViewCatalog.setAdapter(new ProductAdapter(mProductList, getLayoutInflater(), false));
+
+        listViewCatalog.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                Intent productDetailsIntent = new Intent(getBaseContext(),ProductDetailsActivity.class);
+                productDetailsIntent.putExtra(ShoppingCartHelper.PRODUCT_INDEX, position);
+                startActivity(productDetailsIntent);
+            }
+        });
+
+        Button viewShoppingCart = (Button) findViewById(R.id.ButtonViewCart);
+        viewShoppingCart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent viewShoppingCartIntent = new Intent(getBaseContext(), ShoppingCartActivity.class);
+                startActivity(viewShoppingCartIntent);
+            }
+        });
+    }
+}
